d144114c2dec33dd81b3bb8c79b970330b12b605
Jordy Chichanda <jordimendez1999gmail.com>
2025-05-13 20:35:38 -0500

    Login con shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { Button } from "@/components/ui/button"
+import { LoginForm } from "@/components/Login/login-shacn"
 
 function App() {
   return (
     <>
-       <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Soy un botom</Button>
+        <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <LoginForm />
+      </div>
     </div>
     </>
   )
